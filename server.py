ce7afac1e0efbb756fc5156052ac6cae9c6e67fb
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-31 19:19:00 -0700

    working on user page

--- server.py
+++ server.py
@@ -43,26 +43,79 @@ def register_process():
     password = request.form.get('password')
     age = request.form.get('age')
     zip_code = request.form.get('zipcode')
-    app.logger.info(request.form)
-    app.logger.info(db.session.query(User).filter(User.email == email_address).all())
 
+    #debugging prints 
+    #app.logger.info(request.form)
+    #app.logger.info(db.session.query(User).filter(User.email == email_address).all())
 
     # to check if the filtered query exists in the users table  
     if db.session.query(User).filter(User.email == email_address).all():
         message = "Email address already registered to an existing user. Please log in."
-        app.logger.info(str(message))
-        return redirect("/")
+     
+        # debugging to print the message in the debugger log  
+        #app.logger.info(str(message))
+        flash(message)
+        return render_template("log_in.html")
     else:
         #adding the new user to the database 
         new_user = User(email = email_address, password = password, age = age, zipcode = zip_code)
-        app.logger.info(str(new_user))
+
+        #debugging print 
+        #app.logger.info(str(new_user))
+
+        #commiting the new user to the database 
         db.session.add(new_user)
         db.session.commit()
 
+    return redirect("/")
 
+@app.route('/log_in')
+def log_in_form():
+    """render the log in page"""
 
+    return render_template("log_in.html")
+
+@app.route('/log_in',methods=["POST"])
+def log_in_process():
+    """ handle log in with the post data from log_in html"""
+
+    email_address = request.form.get('email')
+    password = request.form.get('password')
+
+    user = db.session.query(User).filter(User.email == email_address).one()
+    if user.password == password:
+        session['current_user_id'] = user.user_id
+        flash (' logged in as {}'.format(email_address))
+
+        return redirect("/")
+    else:
+        flash("wrong password, try again")
+        return redirect('/log_in')
+
+@app.route('/log_out', methods=["POST"])
+def log_out():
+    """ handeling log out """
+    if session:
+        session.clear() 
+        flash("your are logged out")
 
     return redirect("/")
+
+@app.route("/users/<user_id>")
+def show_user_profile(user_id):
+    """show the user detail for the specific user id"""
+
+    user = db.session.query(User).filter(User.user_id == user_id).one()
+    zipcode = user.zipcode
+    age = user.age 
+    #if db.session.query(Rating.user_id).all():
+    #movie_ratings = db.session.query(Rating.movie_id, Rating.movie_score).filter(Rating.user_id == user_id).all()
+
+
+            
+    return render_template("user.html", user=user, zipcode=zipcode, age=age)
+
+
 
 
 if __name__ == "__main__":
